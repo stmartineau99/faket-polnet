f2914b22bfc6afe00da928ab26b47945020d8abd
stmartineau99 <163417329+stmartineau99@users.noreply.github.com>
2026-02-11 15:47:33 +0100

    Update slurm_scripts/submit_polnet_faket.seh
    Added documentation and usage instructions for the script.

--- slurm_scripts/submit_polnet_faket.sh
+++ slurm_scripts/submit_polnet_faket.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+################################################################################
+# Batch submit polnet-faket integrated pipeline jobs for multiple configuration files.
+#
+# Description:
+#   This script iterates over all TOML configuration files in a directory and
+#   submits a separate Slurm job for each one using the `sbatch_polnet_faket.sh`
+#   script.
+#
+# Usage:
+#   1) Set CONFIG_DIR to the folder containing your .toml configuration files.
+#   2) Make sure this script is executable, then run with:
+#        ./submit_polnet_faket.sh
+# 
+# Requirments:
+#   - polnet-synaptic environment
+#   - polnet-faket environment
+################################################################################
+
 CONFIG_DIR=/mnt/lustre-grete/projects/nim00020/sage/data/simulation/deepict_dataset_1/configs
 
 for CONFIG in $CONFIG_DIR/*.toml; do
